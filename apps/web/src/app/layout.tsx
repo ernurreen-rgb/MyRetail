73b4af0c4fcb951d2f4aad21963be6da9d298a0d
Yera All <ernurreen@gmail.com>
2026-06-28 01:09:53 +0500

    Suppress body hydration warning

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
       lang="ru"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
